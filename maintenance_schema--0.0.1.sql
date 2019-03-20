1c2668fd269c9cfbbf3a445e7ccaaa1d6602293d
emerichunter <areyoudeadyet667@yahoo.fr>
2019-03-20 11:45:51 +0100

    duplicate fkeys only 0.0.1

--- maintenance_schema--0.0.1.sql
+++ maintenance_schema--0.0.1.sql
@@ -391,32 +391,33 @@ ORDER BY
 -- Duplicate foreign keys
 CREATE OR REPLACE VIEW maintenance_schema.rpt_fk_duplicate AS
 SELECT
-    pc.conname as constraint_name,
+    array_agg(pc.conname) as duplicated_constraints, 
     pclsc.relname as child_table,
     pac.attname as child_column,
     pclsp.relname as parent_table,
-    pap.attname as parent_column,
+    pap.attname as parent_column,   
     nspname as schema_name
-FROM
+FROM 
     (
     SELECT
-         connamespace,
-                 conname,
-                 unnest(conkey) as "conkey",
-                 unnest(confkey) as "confkey" ,
-                 conrelid,
-                 confrelid,
-                 contype
+            connamespace,
+	    conname, 
+	    unnest(conkey) as "conkey", 
+	    unnest(confkey) as "confkey" , 
+	    conrelid, 
+	    confrelid, 
+	    contype
      FROM
         pg_constraint
     ) pc
     JOIN pg_namespace pn ON pc.connamespace = pn.oid
     JOIN pg_class pclsc ON pc.conrelid = pclsc.oid
-    JOIN pg_class pclsp ON      pc.confrelid = pclsp.oid
-    JOIN pg_attribute pac ON pc.conkey = pac.attnum    and pac.attrelid =       pclsc.oid
+    JOIN pg_class pclsp ON pc.confrelid = pclsp.oid
+    JOIN pg_attribute pac ON pc.conkey = pac.attnum and pac.attrelid = pclsc.oid
     JOIN pg_attribute pap ON pc.confkey = pap.attnum and pap.attrelid = pclsp.oid
-
-ORDER BY pclsc.relname ;
+GROUP BY child_table, child_column, parent_table, parent_column, schema_name 
+HAVING COUNT(*)>1
+ORDER BY child_table, child_column ;
 
 -- Useless unique constraints on FK or PK
 CREATE OR REPLACE VIEW maintenance_schema.rpt_useless_uconstraint AS
